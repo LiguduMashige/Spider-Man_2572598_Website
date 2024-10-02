cb4246b5fe2c21a33ab89bbf4ed2f6b7e81e4322
LiguduMashige <131270266+LiguduMashige@users.noreply.github.com>
2024-10-02 17:47:43 +0200

    nav

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbar');
     // Store pages in an array
     const pages = [
-        { name: 'Home', link: '/index.html' },
-        { name: 'About', link: '/pages/about.html' },
-        { name: 'Services', link: '/pages/services.html' },
-        { name: 'Contact', link: '/pages/contact.html' },
-        { name: 'Design', link: '/pages/design.html'}
+        { name: 'Home', link: 'index.html' },
+        { name: 'About', link: './pages/about.html' },
+        { name: 'Services', link: './pages/services.html' },
+        { name: 'Contact', link: './pages/contact.html' },
+        { name: 'Design', link: './pages/design.html'}
     ];
 
     // Create a <ul> element for the navbar
